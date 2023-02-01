a752dfd72fafaa67f54b3956a5b89375cc3e96da
pthwan27 <parkssuu@gmail.com>
2023-02-02 01:48:18 +0900

    FIX : directory, js file , login

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,16 @@
-import { createApp } from 'vue';
+import { createApp, h } from 'vue';
 import App from './App.vue';
-import router from './router';
+import router from './router/vue-router';
+import store from './store';
+
 import 'bootstrap';
 import './index.css';
 import './assets/main.css';
 
-const app = createApp(App);
-
+const app = createApp({
+	render: () => h(App),
+});
+app.use(store);
 app.use(router);
 
 app.mount('#app');
