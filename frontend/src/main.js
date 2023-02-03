f95b9337754dcc26b5f6ea0057b9444ed81b6bde
pthwan27 <parkssuu@gmail.com>
2023-02-03 14:24:40 +0900

    FIX : main.js, package modify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,10 @@ import App from './App.vue';
 import router from './router/vue-router';
 import store from './store';
 
-import 'bootstrap';
+import BootstrapVueNext from 'bootstrap-vue-next';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
 import './index.css';
 import './assets/main.css';
 
@@ -23,6 +26,7 @@ import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
 library.add(faUserSecret, faBars, faRightToBracket, faUserPlus);
 
 let app = createApp(App);
+app.use(BootstrapVueNext);
 app.use(router);
 app.use(store);
 app.component('fa-icon', FontAwesomeIcon).mount('#app');
