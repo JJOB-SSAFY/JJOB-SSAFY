e676d6f9abef717775bc6e13968be8b120c49242
장진문 <jjm1261@naver.com>
2023-02-06 09:18:07 +0900

    Fix : recruit fix

--- frontend/src/views/main/recruit/components/recruit-recommend.vue
+++ frontend/src/views/main/recruit/components/recruit-recommend.vue
@@ -1,33 +1,25 @@
 <template>
-	<div>
-  <b-card title="Card title" sub-title="Card subtitle">
-    <b-card-text>
-      Some quick example text to build on the <em>card title</em> and make up the bulk of the card's
-      content.
-    </b-card-text>
+  <div>
+    <div>{{ test1 }}</div>
+    <b-card :title="test1" sub-title="Card subtitle">
+      <b-card-text>{{ test1 }} </b-card-text>
 
-    <b-card-text>A second paragraph of text in the card.</b-card-text>
+      <b-card-text>A s</b-card-text>
 
-    <a href="#" class="card-link">Card link</a>
-    <b-link href="#" class="card-link">Another link</b-link>
-  </b-card>
-</div>
+      <a href="#" class="card-link">link</a>
+      <b-link href="#" class="card-link">link</b-link>
+    </b-card>
+  </div>
 </template>
 
 <script>
-import {reactive, onMounted} from 'vue';
-import axios from 'axios';
 export default {
-	setup() {
-		axios({
-			method: 'get',
-			url: 'http://localhost:8080/recruit/list',
-		})
-		.then((res)=>{
-			
-		})
-		return {};
-	},
+  props: {
+    test1: String,
+  },
+  setup() {
+    return {};
+  },
 };
 </script>
 
