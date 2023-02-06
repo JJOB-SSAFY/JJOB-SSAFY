6efdc7202ca1ecce259ef3e6b9f9bffae32bbde2
keeeeeey <sseioul@naver.com>
2023-02-06 21:27:10 +0900

    FIX: MemberResponseDto 수정

--- backend/project/src/main/java/com/ssafy/project/api/response/MemberLoginPostRes.java
+++ backend/project/src/main/java/com/ssafy/project/api/response/MemberLoginPostRes.java
@@ -1,9 +1,13 @@
 package com.ssafy.project.api.response;
 
+import com.ssafy.project.db.entity.MemberRoleEnum;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @NoArgsConstructor
@@ -12,10 +16,14 @@ import lombok.NoArgsConstructor;
 public class MemberLoginPostRes {
 
     private String accessToken;
+    private String name;
+    private String role;
 
-    public static MemberLoginPostRes from(String accessToken) {
+    public static MemberLoginPostRes from(String accessToken, String name, String role) {
         return MemberLoginPostRes.builder()
                 .accessToken(accessToken)
+                .name(name)
+                .role(role)
                 .build();
     }
 
