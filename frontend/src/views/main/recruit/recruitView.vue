45428aea91dec4b9af17b809f98abf36ffb808f7
장진문 <jjm1261@naver.com>
2023-02-06 10:01:57 +0900

    FIX : recruit Item

--- frontend/src/views/main/recruit/recruitView.vue
+++ frontend/src/views/main/recruit/recruitView.vue
@@ -1,49 +1,49 @@
 <template>
-  <!-- <div>채용공고</div> -->
-  <recruit-Recommend :test1="test1" />
+	<!-- <div>채용공고</div> -->
+	<recruit-Recommend :test1="test1" />
 </template>
 
 <script>
-import recruitRecommend from "./components/recruit-recommend.vue";
-import axios from "axios";
+import recruitRecommend from './components/recruit-recommend.vue';
+import axios from 'axios';
 
 export default {
-  name: "recruitView",
+	name: 'recruitView',
 
-  components: {
-    recruitRecommend,
-  },
-  setup() {
-    const test1 = "testtesttesttesttest";
+	components: {
+		recruitRecommend,
+	},
+	setup() {
+		const test1 = 'testtesttesttesttesttesttest';
 
-    const info = {
-      companyName: "",
-      recruitTitle: "",
-      recruitDate: "",
-      work: "",
-      requirement: "",
-    };
+		const info = {
+			companyName: '',
+			recruitTitle: '',
+			recruitDate: '',
+			work: '',
+			requirement: '',
+		};
 
-    const getInfo = function () {
-      axios({
-        method: "get",
-        url: "http://localhost:8080/recruit/list",
-      })
-        .then((res) => {
-          console.log(res);
-          info.companyName = res.data.recruits.companyName;
-          info.recruitTitle = res.data.recruits.recruitTitle;
-          info.recruitDate = res.data.recruits.recruitDate;
-          info.work = res.data.recruits.work;
-          info.requirement = res.data.recruits.requirement;
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    };
+		const getInfo = () => {
+			axios({
+				method: 'get',
+				url: 'http://localhost:8080/recruit/list',
+			})
+				.then(res => {
+					console.log(res);
+					info.companyName = res.data.recruits.companyName;
+					info.recruitTitle = res.data.recruits.recruitTitle;
+					info.recruitDate = res.data.recruits.recruitDate;
+					info.work = res.data.recruits.work;
+					info.requirement = res.data.recruits.requirement;
+				})
+				.catch(err => {
+					console.log(err);
+				});
+		};
 
-    return { getInfo, info, test1 };
-  },
+		return { getInfo, info, test1 };
+	},
 };
 </script>
 
