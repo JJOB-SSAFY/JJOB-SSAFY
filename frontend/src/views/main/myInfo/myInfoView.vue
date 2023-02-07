32ce2058322f3196abd390f107036d24545cccb0
pthwan27 <parkssuu@gmail.com>
2023-02-07 09:22:31 +0900

    FEAT : meinfo test

--- frontend/src/views/main/myInfo/myInfoView.vue
+++ frontend/src/views/main/myInfo/myInfoView.vue
@@ -1,9 +1,19 @@
 <template>
-	<div></div>
+	<div>{{ myInfo }}</div>
 </template>
 
 <script>
-export default {};
+import { reactive, ref } from 'vue';
+import { useStore } from 'vuex';
+
+export default {
+	setup() {
+		const store = useStore();
+		const myInfo = ref(store.getters['auth/getUserInfo']);
+
+		return { myInfo };
+	},
+};
 </script>
 
 <style lang="scss" scoped></style>
