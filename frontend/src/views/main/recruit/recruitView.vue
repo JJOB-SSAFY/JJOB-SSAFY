f3d6f7d68263d1e468925e69cef827352e82391a
장진문 <jjm1261@naver.com>
2023-02-07 15:34:23 +0900

    FIX : recruit fix

--- frontend/src/views/main/recruit/recruitView.vue
+++ frontend/src/views/main/recruit/recruitView.vue
@@ -1,63 +1,78 @@
 <template>
-	<div class="div-recruit-list">
+	<div>
 		<div div-search>
-			<input type="text" v-model.lazy="searchCondition.location" />
-			<input type="text" v-model.lazy="searchCondition.department" />
-			<button>검색</button>
+			<form @submit.prevent="serchInfo">
+				<input
+					type="text"
+					v-model.lazy="state.form.location"
+					placeholder="Location"
+				/>
+				<input
+					type="text"
+					v-model.lazy="state.form.department"
+					placeholder="Department"
+				/>
+				<button>검색</button>
+			</form>
+			<p>{{ searchClick }}</p>
 		</div>
-		<div
+		<!-- <div
 			class="recruit-list-items"
 			v-for="info in state.recruitList"
 			:key="info.recruit"
 		>
 			<recruit-Recommend :info="info" />
-		</div>
+		</div> -->
 	</div>
 </template>
 
 <script>
-import recruitRecommend from './components/recruit-item.vue';
+import recruitItme from './components/recruit-item.vue';
+import { ref, reactive, onMounted, computed } from 'vue';
+import { useStore } from 'vuex';
 import axios from 'axios';
-import { ref, reactive } from 'vue';
 
 export default {
 	name: 'recruitView',
 
 	components: {
-		recruitRecommend,
+		recruitItme,
 	},
 	setup() {
+		const store = useStore();
+
 		const state = reactive({
-			recruitList: '',
-		});
-
-		const searchCondition = reactive({
-			location: '',
-			department: '',
-		});
-
-		const searchClick = () => {
-			const searchInfo = {
-				location: searchCondition.location,
-				department: searchCondition.locationlocation,
-			};
-		};
-		axios({
-			method: 'POST',
-			url: 'http://localhost:8080/recruit/list',
-			headers: {
-				Authorization: localStorage.getItem('jjob.s.token'),
+			form: {
+				location: '',
+				department: '',
 			},
-			data: searchCondition,
-		}).then(res => {
-			state.recruitList = res.data;
-			console.log(searchCondition.department);
 		});
-		// onMounted(() => {
-		// 	store.dispatch('recruit/getList');
+
+		const serchInfo = function () {
+			store.dispatch('recruit/getList', {
+				location: state.form.location,
+				department: state.form.department,
+			});
+		};
+
+		const searchClick = computed(() => {
+			// 조건에 맞는것들만 보이게 해야됌
+			return store.getters['recruit/getRecruitList'];
+		});
+
+		// axios({
+		// 	method: 'POST',
+		// 	url: 'http://localhost:8080/recruit/list',
+		// 	headers: {
+		// 		Authorization: localStorage.getItem('jjob.s.token'),
+		// 	},
+		// 	data: searchCondition,
+		// }).then(res => {
+		// 	state.recruitList = res.data;
+		// 	console.log(searchCondition.department);
 		// });
 
-		return { state, searchCondition, searchClick };
+		return { state, searchClick, serchInfo };
 	},
 };
 </script>
