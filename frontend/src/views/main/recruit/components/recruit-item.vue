edd3aca985bc30e2a642e0e747c55f4437f6e7fa
장진문 <jjm1261@naver.com>
2023-02-08 14:44:22 +0900

    FIX : recruit

--- frontend/src/views/main/recruit/components/recruit-item.vue
+++ frontend/src/views/main/recruit/components/recruit-item.vue
@@ -1,20 +1,24 @@
 <template>
-	<router-link to="recruit/detail">
-		<a href="#">
-			<div class="card">
-				<h5 class="card-header">회사이름{{ info.recruitTitle }}</h5>
+	<div id="items">
+		<div class="card">
+			<h5 class="card-header">회사이름 {{ info.recruitTitle }}</h5>
+			<div class="leftbox">
 				<div class="card-body">
-					<!-- {{ info }} -->
-					<h5 class="card-title">채용직무{{ info.department }}</h5>
-					<p class="card-text">요구사항{{ info.requirement }}</p>
-					<p class="card-text">업무{{ info.workType }}</p>
-					<p class="card-text">경력사항{{ info.work }}</p>
-					<p class="card-text">지원 마감일{{ info.recruitEndDate }}</p>
-					<a href="#" class="btn btn-primary">지원하기 버튼</a>
+					<h6>경력여부 {{ info.work }}</h6>
+				</div>
+				<div class="card-body">
+					<h5 class="card-title">담당업무 {{ info.department }}</h5>
+					<p class="card-text">요구역량 {{ info.requirement }}</p>
 				</div>
 			</div>
-		</a>
-	</router-link>
+			<div class="rightbox">
+				<div class="card-body">
+					<p>지원마감일 {{ info.recruitEndDate }}</p>
+					<a href="#" class="btn btn-primary">상세보기</a>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -29,4 +33,20 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+#items {
+	margin: 15px;
+}
+/* .recruit-item-container {
+	background-color: white;
+	margin: 10px;
+	border-radius: 10px;
+	height: 100px;
+	padding: 10px;
+}
+
+.item-right {
+	display: flex;
+	align-items: right;
+} */
+</style>
