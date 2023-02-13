5d14066eb2409c4c19dc6c44bd7926d723e6f00a
young <gobuig00@naver.com>
2023-02-13 13:51:16 +0900

    recruit

--- frontend/src/api/recruitService.js
+++ frontend/src/api/recruitService.js
@@ -10,4 +10,9 @@ export default class recruitService {
 	async getRecruitDetail(id) {
 		return await http.get(`recruit/list/` + id, config).then(data => data.data);
 	}
+	async getRecruitList(param) {
+		return await http
+			.post(`recruit/list`, param, config)
+			.then(data => data.data);
+	}
 }
